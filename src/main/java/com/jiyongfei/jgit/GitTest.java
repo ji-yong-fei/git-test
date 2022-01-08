1a9bdf9934b40ad337643c997b6e5259d05d724a
“jiyongfei” <2384074837@qq.com>
2022-01-08 23:04:38 +0800

    first commit

---
+++ src/main/java/com/jiyongfei/jgit/GitTest.java
@@ -0,0 +1,11 @@
+package com.jiyongfei.jgit;
+
+/**
+ * @author jiyongfei
+ * @date 2022/1/8 - 22:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
