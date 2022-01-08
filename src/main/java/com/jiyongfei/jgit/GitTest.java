7510caa84c89a533ffbba98daf7198bddf010384
“jiyongfei” <2384074837@qq.com>
2022-01-08 23:09:04 +0800

    second commit

--- src/main/java/com/jiyongfei/jgit/GitTest.java
+++ src/main/java/com/jiyongfei/jgit/GitTest.java
@@ -7,5 +7,6 @@ package com.jiyongfei.jgit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
     }
 }
